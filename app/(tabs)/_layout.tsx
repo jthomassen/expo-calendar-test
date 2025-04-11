b1d8746a8fba0bd34d3a57e56c2ee5f7c0f41727
Jeff Thomassen <jeffkthomassen@gmail.com>
2025-04-11 03:14:12 -0400

    added calendar screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,55 +4,58 @@ import { Ionicons } from "@expo/vector-icons";
 export default function TabsLayout() {
   return (
     <Tabs
-        screenOptions={{
-            tabBarActiveTintColor: '#ffd33d',
-            headerStyle: {
-                backgroundColor: '#25292e',
-            },
-            headerShadowVisible: false,
-            headerTintColor: '#fff',
-            tabBarStyle: {
-                backgroundColor: '#25292e',
-            },
-        }}
+      screenOptions={{
+        tabBarActiveTintColor: '#ffd33d',
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+          headerShadowVisible: false,
+          headerTintColor: '#fff',
+          tabBarStyle: {
+            backgroundColor: '#25292e',
+        },
+      }}
     > 
-        <Tabs.Screen 
-            name="index" 
-            options={{
-                title: "Home",
-                tabBarIcon: ({focused, color}) => (
-                    <Ionicons 
-                        name={focused? "home-sharp" : "home-outline"} 
-                        color={color}
-                        size={30} 
-                    />
-                )
-        }}/>
-        
-        <Tabs.Screen
-            name="about"
-            options={{
-                title: 'About',
-                tabBarIcon: ({ color, focused }) => (
-                    <Ionicons 
-                        name={focused ?               'information-circle' : 'information-circle-outline'} color={color} size={24}
-                    />
-                ),
-        }}
-      />
-        <Tabs.Screen 
-                name="calendar" 
-                options={{
-                    title: 'Calendar',
-                    tabBarIcon: ({ color, focused }) => (
-                      <Ionicons 
-                        name={focused ? 'calendar-outline' : 'calendar-clear-outline'} color={color} size={24}
-                        />
-                    ),
-        }}/>
-        <Tabs.Screen
-            name="not-found"
-            options={{}}/>
+    <Tabs.Screen 
+      name="index" 
+      options={{
+        title: "Home",
+        tabBarIcon: ({focused, color}) => (
+          <Ionicons 
+            name={focused? "home-sharp" : "home-outline"} 
+            color={color}
+            size={30} 
+          />
+        )
+      }}
+    />
+    <Tabs.Screen
+      name="about"
+      options={{
+        title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons 
+              name={focused ? 'information-circle' : 'information-circle-outline'} 
+              color={color} size={24}
+            />
+          ),
+      }}
+    />
+    <Tabs.Screen 
+      name="calendar" 
+      options={{
+        title: 'Calendar',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons 
+              name={focused ? 'calendar-outline' : 'calendar-clear-outline'} 
+              color={color} size={24}
+            />
+          ),
+      }}/>
+    <Tabs.Screen
+      name="not-found"
+      options={{}}
+    />
     </Tabs>
   );
 }
--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,55 +1,40 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text, Button, Platform } from 'react-native';
-import * as Calendar from 'expo-calendar';
+import { Text, View, StyleSheet } from 'react-native';
+import { useState } from "react";
+import { Calendar, toDateId } from "@marceloterreiro/flash-calendar";
 
-export default function App() {
-  useEffect(() => {
-    (async () => {
-      const { status } = await Calendar.requestCalendarPermissionsAsync();
-      if (status === 'granted') {
-        const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
-        console.log('Here are all your calendars:');
-        console.log({ calendars });
-      }
-    })();
-  }, []);
+const today = toDateId(new Date());
 
+export default function CalendarScreen() {
+  const [selectedDate, setSelectedDate] = useState(today);
   return (
     <View style={styles.container}>
-      <Text>Calendar Module Example</Text>
-      <Button title="Create a new calendar" onPress={createCalendar} />
+      <Text style={styles.text}>Selected date: {selectedDate}</Text>
+      <Calendar
+        calendarActiveDateRanges={[
+          {
+            startId: selectedDate,
+            endId: selectedDate,
+          },
+        ]}
+        calendarMonthId={today}
+        onCalendarDayPress={setSelectedDate}
+      />
     </View>
   );
-}
-
-async function getDefaultCalendarSource() {
-  const defaultCalendar = await Calendar.getDefaultCalendarAsync();
-  return defaultCalendar.source;
-}
-
-async function createCalendar() {
-  const defaultCalendarSource =
-    Platform.OS === 'ios'
-      ? await getDefaultCalendarSource()
-      : { isLocalAccount: true, name: 'Expo Calendar' };
-  const newCalendarID = await Calendar.createCalendarAsync({
-    title: 'Expo Calendar',
-    color: 'blue',
-    entityType: Calendar.EntityTypes.EVENT,
-    sourceId: defaultCalendarSource.id,
-    source: defaultCalendarSource,
-    name: 'internalCalendarName',
-    ownerAccount: 'personal',
-    accessLevel: Calendar.CalendarAccessLevel.OWNER,
-  });
-  console.log(`Your new calendar ID is: ${newCalendarID}`);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'center',
+    paddingTop: '40%',
+    paddingBottom: '40%',
+    paddingLeft: '10%',
+    paddingRight: '10%',
+  },
+  text: {
+    color: '#fff',
   },
 });
