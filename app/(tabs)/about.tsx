96e9ed356d311b072aaaccd3d8fb058b2e113ea9
Jeff Thomassen <jeffkthomassen@gmail.com>
2025-04-11 03:17:14 -0400

    "testing source control feature"

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { Text, View,  StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is the calendar app!</Text>
+      <Text style={styles.text}>This is the calendar app</Text>
     </View>
   );
 }
