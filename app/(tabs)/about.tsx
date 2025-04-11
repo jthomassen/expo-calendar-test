5fd4f9057a6c10d22d0ab31183299eba2dc5c2bf
Jeff Thomassen <jeffkthomassen@gmail.com>
2025-04-10 20:07:25 -0400

    first commit

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,10 +1,9 @@
 import { Text, View,  StyleSheet } from 'react-native';
-import { Link } from "expo-router";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to The Calendar App!</Text>
+      <Text style={styles.text}>This is the calendar app!</Text>
     </View>
   );
 }
@@ -18,10 +17,5 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
   },
 });
